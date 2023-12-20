e1cd94a53f6815f2f43e35f2fbec4c8ac53e2612
Jeffrey Wang <jeffrey.19jw@gmail.com>
2023-12-20 16:22:45 -0500

    search and add new convo to db

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,34 +1,87 @@
-import React, { useState } from 'react'
-import { collection, query, where, getDocs } from "firebase/firestore"
+import React, { useContext, useState } from 'react'
+import { collection, query, where, getDocs, setDoc, doc, updateDoc, serverTimestamp, getDoc } from "firebase/firestore"
 import { db } from "../firebase"
+import { AuthContext } from '../context/AuthContext';
 
 export default function Search() {
     const[usersearch, setUsersearch] = useState("");
     const[user, setUser] = useState(null);
     const[error, setError] = useState(false);
     
+    const { currentUser } = useContext(AuthContext);
+    
     async function handleSearch() {
         //search users with a firebase query
         const q = query(collection(db, "users"), where("displayName", "==", usersearch));
 
         try {
-            //execute the query
+            // //execute the query
             const querySnapshot = await getDocs(q);
-            querySnapshot.forEach((doc) => {
-                setUser(doc.data())
-            });
-            console.log(user)
-            if (user === null){
+            // check if any documents are found
+            if (querySnapshot.size === 0) {
+                // no user found
                 setError(true);
+            } else {
+                // user found, get the first document's data
+                const userDoc = querySnapshot.docs[0];
+                setUser(userDoc.data());
+                setError(false);
             }
             
         } catch (error) {
             setError(true);
         }
+        // console.log(error)
     }
 
     function handleKey(e) {
         e.code === "Enter" && handleSearch();
+    }
+
+    async function handleSelect(e) {
+        //add selected user to a chat by creating a new document
+        //first check if there is already an existing chat between the two users (in chats collection)
+        //if not, then create a new chat for the two users using a joined id
+        // and add the id to the convos collection
+        //convos are the list of chats
+        
+        const joinedUid = currentUser.uid > user.uid
+            ? currentUser.uid + user.uid
+            : user.uid + currentUser.uid;
+
+            try {
+                const res = await getDoc(doc(db, "chats", joinedUid));
+                
+                
+                if (!res.exists()){
+                    // create a new chat document
+                    await setDoc(doc (db, "chats", joinedUid), { messages:[] });
+                    
+                    //create users' convos
+                    await updateDoc(doc(db, "convos", currentUser.uid), {
+                        [joinedUid + ".userInfo"]: {
+                            uid: user.uid,
+                            displayName: user.displayName,
+                            photoURL: user.photoURL,
+                        },
+                        [joinedUid + ".date"]: serverTimestamp(),
+                    });
+            
+                    await updateDoc(doc(db, "convos", user.uid), {
+                        [joinedUid + ".userInfo"]: {
+                            uid: currentUser.uid,
+                            displayName: currentUser.displayName,
+                            photoURL: currentUser.photoURL,
+                        },
+                        [joinedUid + ".date"]: serverTimestamp(),
+                    });
+                    
+                }
+            } catch (error) {
+                setUser(null);
+                setUsersearch(usersearch);
+            }
+
     }
 
     return (
@@ -37,12 +90,12 @@ export default function Search() {
                 <input type="text" placeholder='Find chat' onKeyDown={handleKey} onChange={e=>setUsersearch(e.target.value)}/>
             </div>
             {error && <span>User not found!</span>}
-            {user && <div className="users">
+            {user && (<div className="users" onClick={handleSelect}>
                 <img src={user.photoURL} alt="Profile Picture" />
                 <div className="users-info">
                     <span>{user.displayName}</span>
                 </div>
-            </div>}
+            </div>)}
         </div>
     )
 }
