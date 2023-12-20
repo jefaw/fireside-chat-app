4895a69c2e896b0fed1cf37c5f3fc4dade2c30cc
Jeffrey Wang <jeffrey.19jw@gmail.com>
2023-12-20 14:09:28 -0500

    done: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
 
 export default function Login() {
+  const[error, setError] = useState(false);
+  const[errorMsg, setErrorMsg] = useState("");
 
   function handleRedirect(){
     navigate("/register");
   }
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  //login functino
+  async function handleSubmit(e){
+    e.preventDefault();
+    // console.log(e.target[0].value)
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    
+    try {
+      const login = await signInWithEmailAndPassword(auth, email, password)
+      navigate("/")
+    } 
+    catch (error) {
+      setError(true);
+      setErrorMsg(error.message);
+    }
+
+  }
 
   return (
     <>
@@ -15,10 +37,11 @@ export default function Login() {
           <div className="form-wrapper">
               <span className="logo">Fireside Chat</span>
               <span className="wrapper-title">Login</span>
-              <form>
+              <form onSubmit={handleSubmit}>
                   <input type="email" name="" id="email" placeholder="email"/>
                   <input type="password" name="" id="password" placeholder="password"/>
                   <button className="login-btn">Login</button>
+                  {error && <span>*{errorMsg}</span>}
               </form>
               
               <p>Not registered? <a onClick={handleRedirect}>Sign up here</a></p>
